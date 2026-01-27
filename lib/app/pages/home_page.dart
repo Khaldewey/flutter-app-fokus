3403a59d8dc00ad0ebd1aa8c3a8123b52a26a460
Israel Alves <israel_alves77@hotmail.com>
2026-01-27 14:28:30 -0300

    implementar Navegação nomeada
    https://github.com/Khaldewey/flutter-app-fokus/issues/3

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -27,13 +27,7 @@ class HomePage extends StatelessWidget {
                     height: 56,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                TimerPage(timerType: TimerType.focus),
-                          ),
-                        );
+                        Navigator.pushNamed(context, '/timer');
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: AppConfig.buttonColor,
@@ -58,18 +52,13 @@ class HomePage extends StatelessWidget {
                     height: 56,
                     child: ElevatedButton(
                       onPressed: () async {
-                        final result = await Navigator.push<String?>(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                TimerPage(timerType: TimerType.shortBreak),
-                          ),
-                        );
+
+                        final result =  await Navigator.pushNamed(context, '/timer');
 
                         if(result != null){
                           showDialog(context: context, 
                           builder: (context) =>
-                          AlertDialog(title: Text(result),),
+                          AlertDialog(title: Text(result.toString()),),
                           );
                         }
 
@@ -97,13 +86,7 @@ class HomePage extends StatelessWidget {
                     height: 56,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                TimerPage(timerType: TimerType.longBreak),
-                          ),
-                        );
+                        Navigator.pushNamed(context, '/timer');
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: AppConfig.buttonColor,
